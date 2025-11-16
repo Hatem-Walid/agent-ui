443113292aa5a2cccee08c902355f1cc44e6eaed
Hatem-Walid <noter1500@gmail.com>
2025-11-16 06:36:38 +0200

    fix reason of vercle deploy fail

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import AgentDashboard from "./pages/AgentDashboard";
 import AuthPage from "./pages/AuthPage"; 
 import About from "./pages/About";
-import './app.css';
+import './App.css';
 
 
 function App() {
