3fa0f844c93a7e90fef23922fd0188b131f7e541
Hatem-Walid <noter1500@gmail.com>
2025-12-02 13:28:25 +0200

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from "./App";
 import "./index.css";
 import  ToastProvider  from "./components/ToastProvider";// ← أضف ده
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 ReactDOM.createRoot(document.getElementById("root")).render(
+  
   <React.StrictMode>
     <ToastProvider>
       <Router>
         <App />
+        <SpeedInsights/>
       </Router>
     </ToastProvider>
   </React.StrictMode>
