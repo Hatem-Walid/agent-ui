4e83cc503f6f913f7e1eb5c9066569242743d5a2
Hatem-Walid <noter1500@gmail.com>
2025-12-03 20:41:12 +0200

    Logo Name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -159,7 +159,7 @@ const Navbar = () => {
 
           {/* Logo */}
           <div className="logo-container flex items-center md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 text-white font-bold text-lg">
-            🌟 MyLogo
+            VulnSneak
           </div>
 
           {/* 3. Get Started button area - AUTH LOGIC ADDED HERE */}
