dc071468fdc81e968511d0bc275cabbc63a170d3
Hatem-Walid <noter1500@gmail.com>
2025-12-09 21:57:17 +0200

    turn on 404 page joke

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import AuthPage from "./pages/AuthPage"; 
-import About from "./pages/About";
-import SplineChatPage from "./pages/SplineChatPage";
-import FAQ from "./pages/FAQ";
-import Docs from "./pages/Docs";
-import PlanPage from "./pages/PlanPage";
-import Blog from "./pages/Blog";
-import ContactUs from "./pages/ContactUs";
+// import Home from "./pages/Home";
+// import AuthPage from "./pages/AuthPage"; 
+// import About from "./pages/About";
+// import SplineChatPage from "./pages/SplineChatPage";
+// import FAQ from "./pages/FAQ";
+// import Docs from "./pages/Docs";
+// import PlanPage from "./pages/PlanPage";
+// import Blog from "./pages/Blog";
+// import ContactUs from "./pages/ContactUs";
+//import Page404 from "./pages/Page404";
+import Page404joke from "./pages/Page404joke";
 import './App.css';
 
 function App() {
@@ -17,9 +19,9 @@ function App() {
 
   return (
     <>
-      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+      {/* {!hideNavbarRoutes.includes(location.pathname) && <Navbar />} */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/About" element={<About />} />
         <Route path="/ai" element={<SplineChatPage />} />
@@ -27,7 +29,9 @@ function App() {
         <Route path="/doc" element={<Docs />} />
         <Route path="/plan" element={<PlanPage />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/contact" element={<ContactUs />} /> 
+        <Route path="*" element={<Page404 />} />*/}
+        <Route path="*" element={<Page404joke />} />
       </Routes>
     </>
   );
