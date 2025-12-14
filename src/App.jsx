18b8404cbac08932341252b4e4070700f14b18f3
Hatem-Walid <noter1500@gmail.com>
2025-12-15 00:51:24 +0200

    open website

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import AuthPage from "./pages/AuthPage"; 
-// import About from "./pages/About";
-// import SplineChatPage from "./pages/SplineChatPage";
-// import FAQ from "./pages/FAQ";
-// import Docs from "./pages/Docs";
-// import PlanPage from "./pages/PlanPage";
-// import Blog from "./pages/Blog";
-// import ContactUs from "./pages/ContactUs";
-//import Page404 from "./pages/Page404";
+import Home from "./pages/Home";
+import AuthPage from "./pages/AuthPage"; 
+import About from "./pages/About";
+import SplineChatPage from "./pages/SplineChatPage";
+import FAQ from "./pages/FAQ";
+import Docs from "./pages/Docs";
+import PlanPage from "./pages/PlanPage";
+import Blog from "./pages/Blog";
+import ContactUs from "./pages/ContactUs";
+import Page404 from "./pages/Page404";
 import Page404joke from "./pages/Page404joke";
 import './App.css';
 
@@ -19,9 +19,9 @@ function App() {
 
   return (
     <>
-      {/* {!hideNavbarRoutes.includes(location.pathname) && <Navbar />} */}
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
       <Routes>
-        {/* <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/About" element={<About />} />
         <Route path="/ai" element={<SplineChatPage />} />
@@ -30,7 +30,7 @@ function App() {
         <Route path="/plan" element={<PlanPage />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<ContactUs />} /> 
-        <Route path="*" element={<Page404 />} />*/}
+        <Route path="*" element={<Page404 />} />
         <Route path="*" element={<Page404joke />} />
       </Routes>
     </>
